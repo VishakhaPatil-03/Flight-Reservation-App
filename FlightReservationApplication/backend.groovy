47e0652ff845a8cdc2af436907269050f7be1bde
VishakhaPatil <vishakhapatil1441@gmail.com>
2026-03-21 19:23:32 +0530

    backend-updated

---
+++ FlightReservationApplication/backend.groovy
@@ -0,0 +1,18 @@
+pipeline{
+    agent any 
+    stages {
+        stage('Code-Pull'){
+            steps{
+                git branch: 'main', url: 'https://github.com/VishakhaPatil-03/Flight-Reservation-App.git'
+}
+        }
+        stage('Code-Build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    mvn clean package 
+                '''
+            }
+        }
+    }
+}
