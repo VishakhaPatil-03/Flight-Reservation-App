de243b9c289458aafea50f21138cedcf2ee83999
VishakhaPatil <vishakhapatil1441@gmail.com>
2026-03-23 15:16:36 +0530

    Change 2 file backend.groovy and application_properties

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -14,6 +14,43 @@ pipeline{
                 '''
             }
         }
+        stage('Docker-Build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    docker build -t vishupatil14/flight-reservation-pls-18:latest .
+                '''
+            }
+        }
+        stage('QA-Test'){
+            steps{
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
+                 sh '''
+                    cd FlightReservationApplication
+                    mvn sonar:sonar -Dsonar.projectKey=flight-reservation
+                 '''
+                }
+            }
+        }
+        stage('docker-build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    docker build -t vishupatil14/flight-reservation-pls-18:latest .
+                    docker push vishupatil14/flight-reservation-pls-18:latest
+                    docker rmi vishupatil14/flight-reservation-pls-18:latest
+                '''
+            }
+
+        stage('Deploy-to-Kubernetes'){
+            steps{
+                sh '''
+                    kubectl apply -f k8s/
+                  
+                '''
+            }
+        }
+
        
         
     }
